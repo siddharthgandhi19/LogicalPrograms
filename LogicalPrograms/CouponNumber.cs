19068c5aa694900113a8cc5845ac9e17b839c453
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-04 00:54:53 +0530

    [Siddharth] Add: Coupon Number

---
+++ LogicalPrograms/CouponNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace LogicalProblem
+{
+    public class CouponNumber
+    {
+        public static int GenerateRandom()
+        {
+            Random random = new Random();
+            int randomNum = random.Next(0, 11);
+            return randomNum;
+        }
+        public static void GenerateCoupon(int num)
+        {
+            int[] arr = new int[num];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 0;
+                int random = GenerateRandom();
+                for (int j = 0; j < num; j++)
+                {
+                    if (arr[j] == random)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 0)
+                {
+                    arr[i] = random;
+                }
+
+            }
+            foreach (var data in arr)
+            {
+                if (data != 0)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+
+
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -10,7 +10,7 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" +  "6. Exit");
                     
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
@@ -30,6 +30,9 @@ namespace LogicalProblem
                    case 4:
                         Reverse rev = new Reverse();
                         rev.rnum(25);
+                        break;
+                    case 5:
+                        CouponNumber.GenerateCoupon(10);
                         break;
                     default:
                         flag = false;
