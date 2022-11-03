b083b11994f1ec3c83b15e55fd93efafa7261219
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-04 00:32:49 +0530

    [Siddharth]Add:Fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LogicalProblem
+
+{
+    public class FibonacciSeries
+    {
+        public void Print(int num)
+        {
+            int firstNum = 0, secondNum = 1, thirdNum = 0;
+            Console.WriteLine(firstNum + "\n" + secondNum);
+            for (int i = 0; i < num; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                Console.WriteLine(thirdNum);
+            }
+        }
+
+    }
+}
