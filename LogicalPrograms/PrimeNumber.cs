1aa6910dba5b9d86eb74e771b04e7e5f7084d63f
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-04 00:43:53 +0530

    [Siddharth]Add: Prime Number

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LogicalProblem
+{
+    public class PrimeNumber
+    {
+        public void CheckPrime(int num)
+        {
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine("Number is Prime");
+            else
+                Console.WriteLine("Number is not a Prime");
+        }
+    }
+}
