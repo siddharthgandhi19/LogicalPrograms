bc0722bbf4219fea86c1204b1ce009e339d36cec
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-04 01:04:31 +0530

    [Siddharth] Add: Temperature Conversion

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -10,7 +10,7 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" +  "6. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Temperature Conversion" + "\n" + "7. Exit");
                     
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
@@ -33,6 +33,10 @@ namespace LogicalProblem
                         break;
                     case 5:
                         CouponNumber.GenerateCoupon(10);
+                        break;
+                    case 6:
+                        Temperature tem = new Temperature();
+                        tem.temp1();
                         break;
                     default:
                         flag = false;
