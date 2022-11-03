b0676ea04ed0ee513ab3647e06cd05aa745cb80b
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-04 00:39:15 +0530

    [Siddharth]Add: Perfect Number

---
+++ LogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LogicalProblem
+
+{
+    public class PerfectNumber
+    {
+        public void Check(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            if (num == sum)
+                Console.WriteLine("Number provided is Perfect Number");
+            else
+                Console.WriteLine("Number provided is not a Perfect Number");
+        }
+    }
+}
