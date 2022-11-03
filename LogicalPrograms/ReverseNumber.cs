295ead127d9ca687c136a7be39bfc7598021f6ce
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-04 00:50:00 +0530

    [Siddharth]Add: Reverse Number

---
+++ LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LogicalProblem
+{
+    public class Reverse
+    {
+        public void rnum(int num)
+        {
+            int reverse = 0;
+            while (num > 0)
+            {
+                int reminder = num % 10;
+                reverse = reverse * 10 + reminder;
+                num /= 10;
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
