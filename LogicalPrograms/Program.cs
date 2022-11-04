8f778621ba7948310b243586a3a69581dfb35c12
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-04 09:01:35 +0530

    [Siddharth] Add: Stop Watch

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -11,7 +11,7 @@ namespace LogicalProblem
             while (flag)
             {
                 Console.WriteLine("Welcome to Logical Programming Problems");
-                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Temperature Conversion" + "\n" + "7. Day of Week" + "\n" + "8. Exit");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Check Perfect Number" + "\n" + "3. PrimeNumber" + "\n" + "4. Reverse enter" + "\n" + "5. Coupon Number" + "\n" + "6. Temperature Conversion" + "\n" + "7. Day of Week" + "\n" + "8. Stop Watch" + "\n" + "9. Exit");
 
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
@@ -42,6 +42,10 @@ namespace LogicalProblem
                     case 7:
                         Day we = new Day();
                         we.Dat();
+                        break;
+                    case 8:
+                        StopWatch watch = new StopWatch();
+                        watch.Start();
                         break;
                     default:
                         flag = false;
---
+++ LogicalPrograms/StopWatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LogicalProblem
+
+{
+    public class StopWatch
+    {
+        DateTime dateTime = DateTime.Now;
+        public void Start()
+        {
+            int startHr = dateTime.Hour;
+            int StartMin = dateTime.Minute;
+            int startSec = dateTime.Second;
+            int startMilliSec = dateTime.Millisecond;
+            Console.WriteLine("Press s to Start");
+            string data = Console.ReadLine().ToLower();
+            if (data.Equals("s"))
+            {
+                Console.WriteLine(startMilliSec);
+                Console.WriteLine("Press x for Exit");
+                string stop = Console.ReadLine().ToLower();
+                if (stop.Equals("x"))
+                {
+                    DateTime dateTime1 = DateTime.Now;
+                    Console.WriteLine(dateTime1.Millisecond);
+                    int stopHr = dateTime1.Hour;
+                    int stopMin = dateTime1.Minute;
+                    int stopSec = dateTime1.Second;
+                    int stopMilliSec = dateTime1.Millisecond;
+                    Console.WriteLine("Stopwatch details:- Hours:" + (stopHr - startHr) + "Minutes:" + (stopMin - StartMin)
+                        + "Seconds:" + (stopSec - startSec) + "Milliseconds:" + (stopMilliSec - startMilliSec));
+                }
+            }
+        }
+    }
+
+}
